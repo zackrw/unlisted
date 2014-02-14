eebb4620a54e93ebe03394a30377acc60e3da063
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2014-02-14 21:04:28 +0400

    Finish crude sample business webpage.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+
+
+
+  def view
+    @store = {
+      phone: '+71 971 340-3492',
+      name: 'Jake\'s Kitchen',
+      slogan: 'The best food ever',
+      status: 'Jake\'s is currently featuring a two for
+               one special on margaritas.',
+      location: '26 Institute Way',
+      city: 'Abu Dhabi',
+      country: 'UAE',
+      hours: [
+        { title: 'M-F', value: '8am - 9pm' },
+        { title: 'Sa', value: '10am - 7pm' },
+        { title: 'Su', value: '11am - 5pm' }
+      ]
+    }
+    @category = 'food'
+    @tags = 'yummy, tasty, delicious'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Unlisted::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
