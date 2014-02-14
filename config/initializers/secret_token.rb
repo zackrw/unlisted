9e76b664d65b74547a061f39b685c3879c108d04
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2014-02-14 17:56:59 +0400

    Move unlisted back a directory.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unlisted::Application.config.secret_key_base = '0e4a48c5eccf62326d1ed9558e3df189e7ba2a5b37c68c76f6db020e4e8c44673817e81f14876a9e47de0efc5e691f06c606eca7d1bf7fb770409031b672b644'
