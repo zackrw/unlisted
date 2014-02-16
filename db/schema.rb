49157dd1784656df63b8fd558e021cd1fe29c434
Colin Mayer <lestermayer4@gmail.com>
2014-02-15 22:23:56 -0800

    subdomain auto generated as name with non alphanumeric removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215160530) do
+ActiveRecord::Schema.define(version: 20140216055158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140215160530) do
     t.string   "subdomain"
     t.integer  "next"
     t.string   "neighborhood"
+    t.integer  "language"
   end
 
   add_index "stores", ["category_id"], name: "index_stores_on_category_id", using: :btree
